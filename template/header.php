9cac36cbc0d65aca544c35ab9967a6bccacc8869
Zheng Kai <zhengkai@gmail.com>
2012-05-11 10:47:08 +0800

    fix <link ref=me

--- template/header.php
+++ template/header.php
@@ -18,7 +18,7 @@ if (Page::isFollowOnly()) {
 	echo "\n";
 }
 ?>
-<link rel="me" type="text/html" href="//www.google.com/profiles/zheng.kai" />
+<link rel="me" type="text/html" href="http://www.google.com/profiles/zhengkai" />
 <script type="text/javascript">
 var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-98185-1']); _gaq.push(['_trackPageview']); _gaq.push(['_trackPageLoadTime']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = 'https://ssl.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
 </script>
